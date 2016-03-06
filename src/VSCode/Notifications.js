9d125d2efcf3a7b1260d20a9d939510b7eab8c93
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-03-06 17:51:51 +0000

    Fix notification binding for info

--- src/VSCode/Notifications.js
+++ src/VSCode/Notifications.js
@@ -2,6 +2,6 @@
 
 exports.showError = function(s) { return function() { require('vscode').window.showErrorMessage(s); return {}; }; }
 
-exports.showInfo = function(s) { return function() { require('vscode').window.showInfoMessage(s); return {}; }; }
+exports.showInfo = function(s) { return function() { require('vscode').window.showInformationMessage(s); return {}; }; }
 
 exports.showWarning = function(s) { return function() { require('vscode').window.showWarningMessage(s); return {}; }; }
