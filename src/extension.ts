038397610f70cf5fe6b8c5b737a5dacb44b6d790
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-05-14 21:59:01 +0100

    Remove dead code

--- src/extension.ts
+++ src/extension.ts
@@ -1,136 +1,13 @@
 import * as vscode from 'vscode';
-
-import { BuildActionProvider, CodeActionCommands } from './codeActions';
-import { PscError, PscPosition, PscErrorSuggestion, PscResults, QuickFix, FileDiagnostic, BuildResult } from './build';
-
 import { resolve } from 'path';
-
-
 import * as path from 'path';
-
 import { workspace, Disposable, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 import * as lc from 'vscode-languageclient';
 
-
-// const getText = doc => (a,b,c,d) => doc.getText(new vscode.Range(new vscode.Position(a,b), new vscode.Position(c,d)));
-
-
 export function activate(context: vscode.ExtensionContext) {
     const ps = require('./bundle')();
     const config = vscode.workspace.getConfiguration("purescript");
-
-    // const useDoc = (doc : vscode.TextDocument) => {
-    //     if (doc.fileName && doc.fileName.endsWith('.purs')) {
-    //         ps.updateFile(doc.fileName, doc.getText());
-    //     }
-    // };
-    //     .then(() => {
-    //         // if (vscode.window.activeTextEditor) {
-    //         //     useDoc(vscode.window.activeTextEditor.document)
-    //         // }
-    //     })
-    //     .catch((err) => {
-    //         vscode.window.showErrorMessage("Error starting psc-ide");
-    //         console.error(err);
-    //     });
-
-
-    // const diagnosticCollection = vscode.languages.createDiagnosticCollection("purescript");
-    // const buildProvider = new BuildActionProvider();
-    // const onBuildResult = (notify : boolean) => (res : BuildResult) => {
-    //     if (res.success) {
-    //         let code = 0;
-    //         const map = new Map<string, vscode.Diagnostic[]>();
-
-    //         const actionMap = new Map<number, FileDiagnostic>();
-    //         res.diagnostics.forEach(d => {
-    //             ++code;
-    //             d.diagnostic.code = code;
-    //             actionMap.set(code, d);
-                
-    //             const filename = resolve(vscode.workspace.rootPath, d.filename);
-    //             console.log(filename);
-    //             const entries = map.get(filename) || [];
-    //             entries.push(d.diagnostic);
-    //             map.set(filename, entries);
-    //         });
-            
-    //         if (res.quickBuild) {
-    //             diagnosticCollection.set(vscode.Uri.file(res.file), map.get(res.file) || []);
-    //         } else {
-    //             // If I don't clear before set, last error remains when fixed
-    //             diagnosticCollection.clear();
-
-    //             const diags = <[vscode.Uri, vscode.Diagnostic[]][]><Object> Array.from(map.entries()).map(([url, diags]) => [vscode.Uri.file(url), diags]);
-    //             diagnosticCollection.set(diags);
-    //         }
-
-    //         buildProvider.setBuildResults(actionMap);
-
-    //         if (notify) {
-    //             if (res.diagnostics.some(x => x.diagnostic.severity == vscode.DiagnosticSeverity.Error)) {
-    //                 vscode.window.showWarningMessage("Build completed with errors");
-    //             } else {
-    //                 vscode.window.showInformationMessage("Build succeeded");
-    //             }
-    //         }
-            
-    //     } else {
-    //         vscode.window.showErrorMessage("Build error :(");
-    //     }
-    // };
-
-    // vscode.workspace.onDidChangeTextDocument(e => console.log(e.contentChanges));
-    // context.subscriptions.push(
-    //     new vscode.Disposable(ps.deactivate)
-    //   , vscode.window.onDidChangeActiveTextEditor((editor) => {
-    //       if (editor) {
-    //         useDoc(editor.document);
-    //       }
-    //   })
-    //   , vscode.workspace.onDidSaveTextDocument(doc => {
-    //       if (doc.fileName.endsWith(".purs")) {
-    //         if (config.get<boolean>('fastRebuild')) {
-    //             ps.quickBuild(doc.fileName)
-    //                 .then(onBuildResult(false))
-    //                 .catch(err => {
-    //                     console.error(err);
-    //                     vscode.window.showErrorMessage("Rebuild error");
-    //                 })
-    //         }
-    //         useDoc(doc);
-    //       }
-    //     })
-    //   , vscode.languages.registerHoverProvider('purescript', {
-    //     provideHover: (doc, pos, tok) =>
-    //         ps.getTooltips(pos.line, pos.character, getText(doc))
-    //             .then(result => result !== null ? new vscode.Hover(result) : null)
-    //             .catch((err) => {
-    //                 console.error("Hover error", err);
-    //             })
-    //     })
-
-                    // item.command = { 
-                    //     command: "purescript.addCompletionImport", 
-                    //     title: "Add completion import", 
-                    //     arguments: [ pos.line, pos.character, it ] 
-                    // };
-
-    //   , vscode.commands.registerCommand("purescript.build", function() {
-    //       const config = vscode.workspace.getConfiguration("purescript");
-    //       ps.build(config.get<string>("buildCommand"), vscode.workspace.rootPath)
-    //         .then(onBuildResult(true))
-    //         .catch(err => {
-    //             console.error(err);
-    //             vscode.window.showErrorMessage("Build error");
-    //         });
-    //   })
-    //   , vscode.languages.registerCodeActionsProvider('purescript', buildProvider)
-    //   , vscode.Disposable.from(new CodeActionCommands())
-    // );
-
-
     let serverModule = context.asAbsolutePath(path.join('out', 'src', 'server.js'));
     // The debug options for the server
     let debugOptions = { execArgv: ["--nolazy", "--debug=6010"] };
@@ -161,7 +38,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const disposable = client.start();
     client.onReady().then(() => {
-        console.log("started server");
         ps.activate(client)();
     });
     
