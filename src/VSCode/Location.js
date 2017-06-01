6e7f5f39584cefe1c1eca917bd31694dfc990ab6
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-06-01 23:29:42 +0100

    Fix package.jsons

--- src/VSCode/Location.js
+++ src/VSCode/Location.js
@@ -1,5 +1,5 @@
-// module VSCode.Location
 "use strict";
+// module VSCode.Location
 Object.defineProperty(exports, "__esModule", { value: true });
 var vscode_1 = require("vscode");
 exports.mkLocation = function (file) { return function (pos) { return new vscode_1.Location(vscode_1.Uri.file(file), pos); }; };
