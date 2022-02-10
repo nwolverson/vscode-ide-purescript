07905ab8dc7de8f50ddb73c9987d4f3b41c14cba
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2022-02-10 23:35:51 +0000

    Fix middleware test

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -2,20 +2,20 @@ import { middleware, registerMiddleware, unregisterMiddleware } from '../src/mid
 
 test('middleware', () => {
   let res = 0;
-  middleware.didOpen(null, () => {});
+  middleware.didOpen(null, () => Promise.resolve());
   expect(res).toBe(0);
   const myMiddleware = {
-    didOpen: (data, next) => { res++; next(data); }
+    didOpen: (data, next) => { res++; next(data); return Promise.resolve(); }
   }
   registerMiddleware(myMiddleware);
-  middleware.didOpen(null, () => {});
+  middleware.didOpen(null, () => Promise.resolve());
   expect(res).toBe(1);
   registerMiddleware({
-    didOpen: (data, next) => { res += 42; next(data); }
+    didOpen: (data, next) => { res += 42; next(data); return Promise.resolve(); }
   });
-  middleware.didOpen(null, () => {});
+  middleware.didOpen(null, () => Promise.resolve());
   expect(res).toBe(44);
   unregisterMiddleware(myMiddleware);
-  middleware.didOpen(null, () => {});
+  middleware.didOpen(null, () => Promise.resolve());
   expect(res).toBe(86);
 });
